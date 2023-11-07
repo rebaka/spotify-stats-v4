dc6554443465115a8b5dea03ca260a2fc74723a4
rebaka <rtj1302@gmail.com>
2023-11-07 12:09:51 -0800

    Edited Profile and Button

--- client/src/Components/ProfileComponent.tsx
+++ client/src/Components/ProfileComponent.tsx
@@ -11,7 +11,7 @@ function ProfileComponent({profileData, topArtistsData, topTracksData}) {
                 </div>
             )}
 
-            <div className="bg-slate-700 h-[650px] w-[950px] rounded-lg mt-4 flex flex-col items-center">
+            <div className="bg-slate-700 h-[750px] w-[950px] rounded-lg mt-4 flex flex-col items-center">
                 {topArtistsData && (
                 <div className="flex mt-8">
                     <div className="bg-slate-600 rounded-lg mt-8 p-3 mr-5">
@@ -20,39 +20,37 @@ function ProfileComponent({profileData, topArtistsData, topTracksData}) {
                             {topArtistsData.items.slice(0, 5).map((artist) => (
                             <li key={artist.id} className="text-white flex items-center mb-2 text-base">
                                 <img
-                                src={artist.images[0]?.url || 'default-image-url'} 
-                                alt={artist.name} 
-                                className="h-8 w-8 rounded-full mr-2"
+                                    src={artist.images[0]?.url || 'default-image-url'} 
+                                    alt={artist.name} 
+                                    className="h-8 w-8 rounded-full mr-2"
                                 />
                                 {artist.name}
                             </li>
                             ))}
                         </ul>
                     </div>
+
+                    <div className="bg-slate-600 rounded-lg mt-8 p-3 mr-5">
+                        <h2 className="text-white p-2">TOP TRACKS</h2>
+                        <ul>
+                            {topTracksData.items.slice(0, 5).map((track) => (
+                            <li key={track.id} className="text-white flex items-center mb-2 text-base">
+                            <img
+                                src={track.album.images[0]?.url || 'default-image-url'} 
+                                alt={track.name} 
+                                className="h-8 w-8 object-cover mr-2" 
+                            />
+                                {track.name}
+                            </li>
+                            ))}
+                        </ul>
+                    </div>
+
+                    <button></button>
                 </div>
                 )}
             </div>
         </div>
-
-    //             {/* Top Tracks */}
-    //             <div className="bg-slate-600 rounded-lg mt-8 p-3 mr-5 w-[250px]">
-    //                 <h2 className="text-white p-2">TOP TRACKS</h2>
-    //                 <ul>
-    //                     {topTracksData.items.slice(0, 5).map((track) => (
-    //                     <li key={track.id} className="text-white flex items-center mb-2 text-base">
-    //                         <img
-    //                         src={track.album.images[0]?.url || 'default-image-url'} 
-    //                         alt={track.name} 
-    //                         className="h-8 w-8 object-cover mr-2" 
-    //                         />
-    //                         {track.name}
-    //                     </li>
-    //                     ))}
-    //                 </ul>
-    //             </div>
-    //         </div>
-    //         )}
-    //     </div>
     );
 };
 
