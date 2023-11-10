e0d6e05d1c807da7880b5f6e72985065c519b01a
rebaka <rtj1302@gmail.com>
2023-11-10 01:03:06 -0800

    Added artist names to tracks

--- client/src/Components/ProfileComponent.tsx
+++ client/src/Components/ProfileComponent.tsx
@@ -3,7 +3,6 @@ import TopArtistsComponent from './TopArtistsComponent.tsx';
 import TopTracksComponent from './TopTracksComponent.tsx';
 
 function ProfileComponent({profileData, topArtistsData, topTracksData}) {
-    
     return (
         <div className='flex flex-hor items-center' style={{ alignItems: 'flex-start' }}>
             {profileData && (
@@ -15,30 +14,23 @@ function ProfileComponent({profileData, topArtistsData, topTracksData}) {
             )}
 
             <div className="bg-slate-700 h-[750px] w-[950px] rounded-lg mt-4 flex flex-col items-center ">
-                <h2 className="text-white p-2 mt-4">TOP ARTISTS</h2>
+                {/* <h2 className="text-white p-2 mt-4">TOP ARTISTS</h2> */}
+                <h2 className="text-white p-2 mt-4">TOP TRACKS</h2>
                 {topArtistsData && (
                 <div className="flex mt-4 mb-4 overflow-auto">
-                    <ul>
+                    {/* <ul>
                         {topArtistsData.items.slice(0, 20).map((artist) => (
                             <TopArtistsComponent key={artist.id} topArtistsData={artist} />
                         ))}
-                    </ul>
+                    </ul> */}
 
-                    {/* <div className="bg-slate-600 rounded-lg mt-8 p-3 mr-5">
-                        <h2 className="text-white p-2">TOP TRACKS</h2>
+                    {/* <div className="bg-slate-600 rounded-lg mt-8 p-3 mr-5"> */}
                         <ul>
-                            {topTracksData?.items?.slice(0, 5).map((track) => (
-                            <li key={track.id} className="text-white flex items-center mb-2 text-base">
-                            <img
-                                src={track.album.images[0]?.url || 'default-image-url'} 
-                                alt={track.name} 
-                                className="h-8 w-8 object-cover mr-2" 
-                            />
-                                {track.name}
-                            </li>
+                            {topTracksData?.items?.slice(0, 20).map((track) => (
+                                <TopTracksComponent key={track.id} topTracksData={track}/>
                             ))}
                         </ul>
-                    </div>  */}
+                    {/* </div>  */}
 
                     {/* <div className="bg-slate-600 rounded-lg mt-8 p-3 mr-5">
 
