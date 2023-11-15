68b18076eba965e911b9ff2ff8144a9fd691bc3d
rebaka <rtj1302@gmail.com>
2023-11-15 01:48:08 -0800

    Added title, modified tabs

--- client/src/Components/ProfileComponent.tsx
+++ client/src/Components/ProfileComponent.tsx
@@ -21,64 +21,59 @@ function ProfileComponent({profileData, topArtistsData, topTracksData}) {
             )}
 
             {/* for tabs */}
-            <div className='flex flex-col items-center'>
-                <div className='flex mt-8 border-b border-gray-200 dark:border-gray-700'>
+            <div className='flex flex-col'>
+                <div className='flex mt-4 border-b border-gray-200 dark:border-gray-700'>
                     <nav className='flex space-x-2' aria-label='Tabs' role='tablist'>
-                        <button onClick={() => handleButtonClick('tracks')} type='button' className='hs-tab-active:bg-white hs-tab-active:border-b-transparent
-                        hs-tab-active:text-blue-600 dark:hs-tab-active:bg-gray-800 dark:hs-tab-active:border-b-gray-800 
-                        dark:hs-tab-active:text-white -mb-px py-3 px-4 inline-flex items-center gap-x-2 bg-gray-50 text-lg font-large text-center 
-                        border text-gray-500 rounded-t-lg hover:text-gray-700 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-700 dark:border-gray-700 
+                        <button onClick={() => handleButtonClick('tracks')} type='button' className='hs-tab-active:bg-slate-700 hs-tab-active:border-b-transparent
+                        hs-tab-active:text-white dark:hs-tab-active:bg-gray-800 dark:hs-tab-active:border-b-gray-800 
+                        dark:hs-tab-active:text-white -mb-px py-3 px-4 inline-flex items-center gap-x-2 bg-slate-700 text-lg font-large text-center 
+                        border text-gray-500 rounded-t-lg hover:text-white disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-700 dark:border-gray-700 
                         dark:text-gray-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 active' id='tab-top-tracks-item' data-hs-tab='tab-top-tracks-tab'
                         aria-controls='card-type-tab-1' role='tab'>
-                            Top Tracks
+                            TOP TRACKS
                         </button>
 
-                        <button onClick={() => handleButtonClick('artists')} type='button' className='hs-tab-active:bg-white hs-tab-active:border-b-transparent
-                        hs-tab-active:text-blue-600 dark:hs-tab-active:bg-gray-800 dark:hs-tab-active:border-b-gray-800 
-                        dark:hs-tab-active:text-white -mb-px py-3 px-4 inline-flex items-center gap-x-2 bg-gray-50 text-lg font-large text-center 
-                        border text-gray-500 rounded-t-lg hover:text-gray-700 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-700 dark:border-gray-700 
+                        <button onClick={() => handleButtonClick('artists')} type='button' className='hs-tab-active:bg-slate-700 hs-tab-active:border-b-transparent
+                        hs-tab-active:text-pink-500 dark:hs-tab-active:bg-gray-800 dark:hs-tab-active:border-b-gray-800 
+                        dark:hs-tab-active:text-white -mb-px py-3 px-4 inline-flex items-center gap-x-2 bg-slate-700 text-lg font-large text-center 
+                        border text-gray-500 rounded-t-lg hover:text-white disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-700 dark:border-gray-700 
                         dark:text-gray-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 active' id='tab-top-artists-item' data-hs-tab='tab-top-artists-tab'
                         aria-controls='card-type-tab-2' role='tab'>
-                            Top Tracks
+                            TOP ARTISTS
                         </button>
                     </nav>
                 </div>
 
-                <div className='bg-slate-700 h-[750px] w-[950px] rounded-lg mt-4 flex flex-col items-center'>
-                    {/* <div className='flex mt-8'>
-                        <button onClick={() => handleButtonClick('tracks')} className='mr-2'>
-                            TOP TRACKS
-                        </button>
-                        <button onClick={() => handleButtonClick('artists')} className='mr-2'>
-                            TOP ARTISTS
-                        </button>
-                    </div> */}
-
+                <div className='bg-slate-700 h-[750px] w-[950px] rounded-lg flex flex-col items-center'>
                     {displayMode === 'tracks' && topTracksData && (
                         <div className='overflow-auto'>
-                            <h2 className="text-white p-2">TOP TRACKS</h2>
-                            <ul>
-                            {topTracksData.items.slice(0, 10).map((track) => (
-                                <TopTracksComponent key={track.id} topTracksData={track} />
-                            ))}
-                            </ul>
+                            <h2 className='sticky top-0 z-10 bg-slate-700 p-2 text-center'>TOP TRACKS</h2>
+                            <div className='relative' style={{ paddingTop: '0.45rem' }}>
+                                <ul>
+                                {topTracksData.items.slice(0, 10).map((track) => (
+                                    <TopTracksComponent key={track.id} topTracksData={track} />
+                                ))}
+                                </ul>
+                            </div>
                         </div>
                     )}
 
                     {displayMode === 'artists' && topArtistsData && (
                         <div className='overflow-auto'>
-                            <h2 className="text-white p-2">TOP ARTISTS</h2>
-                            <ul>
-                            {topArtistsData.items.slice(0, 10).map((artist) => (
-                                <TopArtistsComponent key={artist.id} topArtistsData={artist} />
-                            ))}
-                            </ul>
+                            <h2 className='sticky top-0 z-10 bg-slate-700 p-2 text-center'>TOP ARTISTS</h2>
+                            <div className='relative' style={{ paddingTop: '0.45rem' }}>
+                                <ul>
+                                    {topArtistsData.items.slice(0, 10).map((artist) => (
+                                        <TopArtistsComponent key={artist.id} topArtistsData={artist} />
+                                    ))}
+                                </ul>
+                            </div>
                         </div>
                     )}
                 </div>
-            </div>
 
-            
+                <div style={{ paddingBottom: '2rem' }}></div>
+            </div>  
         </div>
     );
 };
