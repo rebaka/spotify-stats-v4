4d91243bf3dbfa5498ba3031612b5aebcb6ae9eb
rebaka <rtj1302@gmail.com>
2023-11-14 18:15:47 -0800

    Added tabs

--- client/src/Components/ProfileComponent.tsx
+++ client/src/Components/ProfileComponent.tsx
@@ -1,8 +1,15 @@
 import UserProfileComponent from './UserProfileComponent.tsx';
 import TopArtistsComponent from './TopArtistsComponent.tsx';
 import TopTracksComponent from './TopTracksComponent.tsx';
+import { useState } from 'react';
 
 function ProfileComponent({profileData, topArtistsData, topTracksData}) {
+    const [displayMode, setDisplayMode] = useState('tracks');
+
+    const handleButtonClick = (mode) => {
+        setDisplayMode(mode)
+    };
+
     return (
         <div className='flex flex-hor items-center' style={{ alignItems: 'flex-start' }}>
             {profileData && (
@@ -13,34 +20,65 @@ function ProfileComponent({profileData, topArtistsData, topTracksData}) {
                 </div>
             )}
 
-            <div className="bg-slate-700 h-[750px] w-[950px] rounded-lg mt-4 flex flex-col items-center ">
-                {/* <h2 className="text-white p-2 mt-4">TOP ARTISTS</h2> */}
-                <h2 className="text-white p-2 mt-4">TOP TRACKS</h2>
-                {topArtistsData && (
-                <div className="flex mt-4 mb-4 overflow-auto">
-                    {/* <ul>
-                        {topArtistsData.items.slice(0, 20).map((artist) => (
-                            <TopArtistsComponent key={artist.id} topArtistsData={artist} />
-                        ))}
-                    </ul> */}
+            {/* for tabs */}
+            <div className='flex flex-col items-center'>
+                <div className='flex mt-8 border-b border-gray-200 dark:border-gray-700'>
+                    <nav className='flex space-x-2' aria-label='Tabs' role='tablist'>
+                        <button onClick={() => handleButtonClick('tracks')} type='button' className='hs-tab-active:bg-white hs-tab-active:border-b-transparent
+                        hs-tab-active:text-blue-600 dark:hs-tab-active:bg-gray-800 dark:hs-tab-active:border-b-gray-800 
+                        dark:hs-tab-active:text-white -mb-px py-3 px-4 inline-flex items-center gap-x-2 bg-gray-50 text-lg font-large text-center 
+                        border text-gray-500 rounded-t-lg hover:text-gray-700 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-700 dark:border-gray-700 
+                        dark:text-gray-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 active' id='tab-top-tracks-item' data-hs-tab='tab-top-tracks-tab'
+                        aria-controls='card-type-tab-1' role='tab'>
+                            Top Tracks
+                        </button>
 
-                    <ul>
-                        {topTracksData?.items?.slice(0, 20).map((track) => (
-                            <TopTracksComponent key={track.id} topTracksData={track}/>
-                        ))}
-                    </ul>
+                        <button onClick={() => handleButtonClick('artists')} type='button' className='hs-tab-active:bg-white hs-tab-active:border-b-transparent
+                        hs-tab-active:text-blue-600 dark:hs-tab-active:bg-gray-800 dark:hs-tab-active:border-b-gray-800 
+                        dark:hs-tab-active:text-white -mb-px py-3 px-4 inline-flex items-center gap-x-2 bg-gray-50 text-lg font-large text-center 
+                        border text-gray-500 rounded-t-lg hover:text-gray-700 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-700 dark:border-gray-700 
+                        dark:text-gray-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 active' id='tab-top-artists-item' data-hs-tab='tab-top-artists-tab'
+                        aria-controls='card-type-tab-2' role='tab'>
+                            Top Tracks
+                        </button>
+                    </nav>
+                </div>
 
-                    {/* <div className="bg-slate-600 rounded-lg mt-8 p-3 mr-5">
-
+                <div className='bg-slate-700 h-[750px] w-[950px] rounded-lg mt-4 flex flex-col items-center'>
+                    {/* <div className='flex mt-8'>
+                        <button onClick={() => handleButtonClick('tracks')} className='mr-2'>
+                            TOP TRACKS
+                        </button>
+                        <button onClick={() => handleButtonClick('artists')} className='mr-2'>
+                            TOP ARTISTS
+                        </button>
                     </div> */}
 
-                    {/* <button></button> */}
+                    {displayMode === 'tracks' && topTracksData && (
+                        <div className='overflow-auto'>
+                            <h2 className="text-white p-2">TOP TRACKS</h2>
+                            <ul>
+                            {topTracksData.items.slice(0, 10).map((track) => (
+                                <TopTracksComponent key={track.id} topTracksData={track} />
+                            ))}
+                            </ul>
+                        </div>
+                    )}
+
+                    {displayMode === 'artists' && topArtistsData && (
+                        <div className='overflow-auto'>
+                            <h2 className="text-white p-2">TOP ARTISTS</h2>
+                            <ul>
+                            {topArtistsData.items.slice(0, 10).map((artist) => (
+                                <TopArtistsComponent key={artist.id} topArtistsData={artist} />
+                            ))}
+                            </ul>
+                        </div>
+                    )}
                 </div>
-                )}
             </div>
+
             
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Top Artists</button>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Top Tracks</button>
         </div>
     );
 };
--- client/src/Components/TopArtistsComponent.tsx
+++ client/src/Components/TopArtistsComponent.tsx
@@ -9,11 +9,11 @@ interface TopArtistsComponentProps {
 
 function TopArtistsComponent({topArtistsData}: TopArtistsComponentProps) {
     return(
-        <div className="h-[70px] w-[900px] bg-black rounded-lg mb-4 p-4 flex ">
+        <div className='h-[75px] w-[900px] bg-black rounded-lg mb-4 p-4 ml-4 mr-4 flex items-center'>
             <img
                 src={topArtistsData.images[0]?.url || 'default-image-url'}
                 alt={topArtistsData.name}
-                className="h-11 w-11 rounded-full mr-2 self-center"
+                className="h-12 w-12 rounded-full mr-2 self-center"
             />
             <span className="text-white flex-grow ml-2">{topArtistsData.name}</span>
       </div>
