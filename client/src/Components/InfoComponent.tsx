256578db95e5be3c938bf88bfd58e69704e27605
rebaka <rtj1302@gmail.com>
2023-12-05 16:55:07 -0800

    Added icons and github link

--- client/src/Components/InfoComponent.tsx
+++ client/src/Components/InfoComponent.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import CloseIcon from '@mui/icons-material/Close';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import Button from '@mui/material/Button';
 
 interface InfoComponentProps {
   isOpen: boolean;
@@ -12,13 +15,21 @@ const InfoComponent: React.FC<InfoComponentProps> = ({ isOpen, onClose }) => {
         <>
           <div
             className="fixed top-0 left-0 w-full h-full bg-black opacity-50"
-            onClick={onClose}
+
           ></div>
 
-          <div className={`fixed inset-0 flex items-center justify-center ${isOpen ? 'visible' : 'invisible'}`} onClick={onClose}>
-            <div className='bg-slate-800 text-white p-12 rounded-lg fixed z-50' onClick={(e) => e.stopPropagation()}>
-              <p className='text-xl font-bold mb-4 h-50 w-45'>SPOTIFY STATS helps to keep track of your most played artists, tracks, and recently played songs. </p>
-              <p className='text-gray-1000'>Click outside to close</p>
+          <div className={`fixed inset-0 flex items-center justify-center ${isOpen ? 'visible' : 'invisible'}`} >
+            <div className='bg-slate-800 text-white p-16 rounded-lg fixed z-50 '>
+              <Button className='absolute -left-14 -top-12' onClick={onClose}>
+                <CloseIcon className='text-white' fontSize='large'/>
+              </Button>
+              <p className='text-2xl font-bold mb-4'>SPOTIFY STATS helps to keep track of your most played artists, tracks, and recently played songs. </p>
+              <p className='text-xl font-bold mb-4'>Check out the Github link below:</p>
+              <a href={'https://github.com/rebaka/spotify-stats-v4'} target="_blank" rel="noopener noreferrer">
+                <Button>
+                  <GitHubIcon className='text-white' fontSize='large'/>
+                </Button>
+              </a>
             </div>
           </div>
         </>
